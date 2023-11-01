18aeb5f12b32c6f6cb2628462fc76c065dac9ba9
atPavithran <143920908+atPavithran@users.noreply.github.com>
2023-11-01 09:05:26 +0530

    Update website.py

--- website.py
+++ website.py
@@ -29,11 +29,7 @@ for item_name, details in menu.items():
             st.markdown(f"<h3 style='text-align: center; color: pink'>{item_name}</h3>", unsafe_allow_html=True)
             st.write("Price:", details['price'])
             image = Image.open(details['image'])
-            st.write(
-    f"<div style='display: flex; justify-content: center;'><img src='{details['image']}' width='300' /></div>",
-    unsafe_allow_html=True
-
-            )            
+            st.image(image, width=200)          
             quantity = st.slider(f"Select quantity of: {item_name}", 0, 10, 0)
             if quantity > 0:
                 cart_items.append((item_name, quantity, menu[item_name]["price"]))
